13d6b59420d55e9f0dce7b4e5d5bfdf1d12e2504
Peter.Li <yeshuanova@gmail.com>
2015-07-02 17:15:39 +0800

    Change kernel name. Modify README.md file.

--- OpenCL.AddValue/main.cpp
+++ OpenCL.AddValue/main.cpp
@@ -22,7 +22,7 @@ __kernel void add_float(__global const float* a, __global const float* b, __glob
     int idx = get_global_id(0); \
     result[idx] = a[idx] + b[idx]; \
 } \
-__kernel void multiple_float(__global const float* a, __global const float* b, __global float* result) { \
+__kernel void multiply_float(__global const float* a, __global const float* b, __global float* result) { \
 int idx = get_global_id(0); \
 result[idx] = a[idx] * b[idx]; \
 } \
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OpenCL.AddValue
+
+This project show a simple example that processes data using OpenCL. We create two OpenCL kernels for adding and multiplying random value, and display result in command line finally.
